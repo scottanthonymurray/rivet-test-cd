437c5617a49247f96a27e97cea6658b5e93fce25
Scott Anthony Murray <scanmurr@iu.edu>
2021-02-23 10:22:23 -0500

    Initial commit

---
+++ actions/build-changelog/index.js
@@ -0,0 +1,27 @@
+const { GitHub } = require('@actions/github');
+const core = require('@actions/core');
+
+async function run() {
+  try {
+    const myToken = core.getInput('GITHUB_TOKEN');
+    const version = core.getInput('version', { required: true });
+    const query = `type:pr+label:${version}`;
+    const octokit = new GitHub(myToken);
+
+    const { data: pullRequest } = await octokit.search.issuesAndPullRequests({
+      q: query
+    });
+
+    let changelog = '';
+
+    pullRequest.items.forEach(function(item) {
+      changelog += `- [${item.title}](${item.html_url})\n`;
+    });
+
+    core.setOutput('changelog', changelog);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
